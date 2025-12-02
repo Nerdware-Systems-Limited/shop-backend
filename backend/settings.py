12eaf23fa2718b7f5b01a261a7456faccd55e9bd
tricrck <tricrck@gmail.com>
2025-12-02 12:22:23 +0300

    Update allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -29,12 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-9)mq-mu*y_f$#b))#a&j8vev^g1z83d#fybqm^z+vyrw)ojo3+')
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='shop-backend-9dtg.onrender.com,localhost,127.0.0.1').split(',')
 
 
 # Application definition
